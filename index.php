3c3f0fe59f2235cde29cbd629726c4ed81e9a125
hibeasdasd <105937764+hibeasdasd@users.noreply.github.com>
2023-03-03 22:09:54 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to my site</title>
+</head>
+<body>
+	<h1>Welcome to my site</h1>
+	<p>This is a simple welcome page hosted on GitHub Pages.</p>
+</body>
+</html>
